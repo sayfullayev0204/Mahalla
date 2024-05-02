d45e3a6399c4bd95c24d3021f8a2d06fb2953f0a
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-05-02 21:07:38 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -191,7 +191,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
+    "https://mahalla-0z5h.onrender.com",
     "https://sub.example.com",
     "http://127.0.0.1:8000",
 ]
