c3614695c5d01f629c044c497352f282707c8489
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-05-03 10:35:46 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -26,17 +26,17 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("adduser.urls")),
+    path("adduser", include("adduser.urls")),
     path("accounts/", include("accounts.urls")),
     path("monitoring/", include("monitoring.urls")),
     path("statistika/", include("statistika.urls")),
-    path("", include("excell_tables.urls")),
+    path("excel", include("excell_tables.urls")),
     # API documentation
     path(
         "swagger.<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
     path(
-        "swagger/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
